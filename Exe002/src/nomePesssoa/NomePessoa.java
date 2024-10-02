02c603e9c4f7b08fcc687fbd011d6c5d1aa7a593
WJESUSS <162203970+WJESUSS@users.noreply.github.com>
2024-10-01 21:15:43 -0300

    Nome_pessoa

---
+++ Exe002/src/nomePesssoa/NomePessoa.java
@@ -0,0 +1,18 @@
+package nomePesssoa;
+
+import java.util.Scanner;
+
+public class NomePessoa {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	System.out.println("Digite seu nome:");
+	String nome =  sc.nextLine();
+	
+	
+	System.out.println("O meu nome "+nome);
+	
+
+	}
+
+}
