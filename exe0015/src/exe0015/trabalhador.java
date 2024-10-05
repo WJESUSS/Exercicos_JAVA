a2857536ae9bcbdb1beffa3d291ffccb4f8c3561
WJESUSS <162203970+WJESUSS@users.noreply.github.com>
2024-10-05 11:54:32 -0300

    SalarioTrabalhador

---
+++ exe0015/src/exe0015/trabalhador.java
@@ -0,0 +1,22 @@
+package exe0015;
+
+import java.util.Scanner;
+
+public class trabalhador {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite o dias de trabalho do funcionário : ");
+		int dias=sc.nextInt();
+		
+		int horasDiaria = 8;
+		double valorHora =25;
+		
+		double  salario =dias*horasDiaria*valorHora;
+		
+		System.out.println("O salario do funcionario é de "+salario);
+
+	}
+
+}
