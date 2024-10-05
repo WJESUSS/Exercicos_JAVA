ee26570637df8144a8353f73dbd36265451dbd3f
WJESUSS <162203970+WJESUSS@users.noreply.github.com>
2024-10-05 10:58:07 -0300

    equacaçaoDoSegundoGrau

---
+++ exe0011/src/exe0011/equacaoSegundoGrau.java
@@ -0,0 +1,24 @@
+package exe0011;
+
+import java.util.Scanner;
+
+public class equacaoSegundoGrau {
+
+	public static void main(String[] args) {
+		
+		Scanner sc =  new Scanner(System.in);
+		
+		System.out.println("Digite o numero A:");
+		int A =sc.nextInt();
+		System.out.println("Digite o numero B :");
+		int B = sc.nextInt();
+		System.out.println("Digite o numero C :");
+		int C = sc.nextInt();
+		
+		int delta = B*B-4*A*C;
+		
+		System.out.println("O valor de delta é " +delta);
+
+	}
+
+}
