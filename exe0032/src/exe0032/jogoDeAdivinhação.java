cda43b020d9d0879041a459234e390ad270f670f
WJESUSS <162203970+WJESUSS@users.noreply.github.com>
2024-10-07 15:23:54 -0300

    JogoAdivinhação

--- exe0032/src/exe0032/jogoDeAdivinhação.java
+++ exe0032/src/exe0032/jogoDeAdivinhação.java
@@ -8,27 +8,19 @@ public class jogoDeAdivinhação {
 	public static void main(String[] args) {
 		
 		Scanner sc=new Scanner(System.in);
-		int [] opcao = {0,1,2,3,4};
+	
 		Random randon = new Random();
+     
+		int NumeroSerSorteado = randon.nextInt(5)+1;
 		
-		System.out.println("Digite um número a ser adivinhado :");
-		System.out.print("Qual é sua escolha :");
-		int numero = sc.nextInt();
-		int numeroSerAdivinhado = randon.nextInt(5);
+		System.out.print("Digite um numero entre (1 a 5)");
+		int papite = sc.nextInt();
 		
-		System.out.println("Escolha do jogador " +opcao[numero]);
-		System.out.println("Escolhar do computador " +opcao[numeroSerAdivinhado]);
-		
-		
-	 if(opcao[numero]==opcao[numeroSerAdivinhado]) {
-		 System.out.println("Houve empate ");
-	 }else if(opcao[numero]>opcao[numeroSerAdivinhado]) {
-		 System.out.println("O jogador ganhou");
-	 }else if(opcao[numero] < opcao[numeroSerAdivinhado]) {
-		 System.out.println("O computador ganhou !");
-	 }
-		
-
+		if(papite==NumeroSerSorteado) {
+			System.out.println("Voce acertou o numero ");
+		}else {
+			System.out.println("Voce nao acertou,tente novamente mais tarde!");
+		}
 	}
-
 }
+
