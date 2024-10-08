fd3b2bd831329e5f1beebc1c33dbfce6a9f2836e
WJESUSS <162203970+WJESUSS@users.noreply.github.com>
2024-10-07 21:24:32 -0300

    For_MultiploDe3

---
+++ exe0040/src/exe0040/forMultiplo3.java
@@ -0,0 +1,13 @@
+package exe0040;
+
+public class forMultiplo3 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 21; i+=3) {
+			System.out.println("O os multiplo de 3 = " +i);
+		}
+
+	}
+
+}
