505ce554c20f97547a90a09895eb45ac34f2e673
WJESUSS <162203970+WJESUSS@users.noreply.github.com>
2024-10-07 21:15:43 -0300

    For_Contagem

---
+++ exe0038/src/exe0038/contagemFor.java
@@ -0,0 +1,20 @@
+package exe0038;
+
+import java.util.Scanner;
+
+public class contagemFor {
+
+	public static void main(String[] args) {
+		
+ 
+		Scanner sc =new Scanner(System.in);
+		
+		
+		
+		
+		for (int i = 6; i < 12; i++) {
+			System.out.println("Os numero " +i);
+		}
+	}
+
+}
