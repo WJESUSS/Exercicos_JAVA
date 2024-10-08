89915eb2220d232ca64293272df480cc9427ea61
WJESUSS <162203970+WJESUSS@users.noreply.github.com>
2024-10-07 21:20:43 -0300

    For_decremento

---
+++ exe0039/src/exe0039/forContagemDescrementar.java
@@ -0,0 +1,19 @@
+package exe0039;
+
+import java.util.Scanner;
+
+public class forContagemDescrementar {
+
+	public static void main(String[] args) {
+	
+		Scanner sc =  new Scanner(System.in);
+		
+		
+		
+		for (int i = 10; i > 2; i--) {
+			System.out.println("O descremento " +i);
+		}
+
+	}
+
+}
