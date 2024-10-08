6d528287bf92388eabdb4775424ec05a6e7ba580
WJESUSS <162203970+WJESUSS@users.noreply.github.com>
2024-10-07 21:27:21 -0300

    For_Multiplo5

---
+++ exe0041/src/exe0041/for_Multiplo5.java
@@ -0,0 +1,13 @@
+package exe0041;
+
+public class for_Multiplo5 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 100; i > 0; i-=5) {
+			System.out.println("O os multiplo de 5 = " +i);
+
+		}
+	}
+
+}
