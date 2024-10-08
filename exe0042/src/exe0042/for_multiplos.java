6205e38c4feee4a4e19ba3014f00a814aa58de29
WJESUSS <162203970+WJESUSS@users.noreply.github.com>
2024-10-07 21:30:47 -0300

    For_Multiplos

---
+++ exe0042/src/exe0042/for_multiplos.java
@@ -0,0 +1,20 @@
+package exe0042;
+
+import java.util.Scanner;
+
+public class for_multiplos {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite um numero :");
+		int numero = sc.nextInt();
+		
+		for (int i = 0; i <= numero; i++) {
+			System.out.println("O numero digitado  = " +i);
+
+	}
+
+}
+}
