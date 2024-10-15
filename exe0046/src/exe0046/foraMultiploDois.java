d41502ecfc1681799035a607f5fb5fcfa6771c0a
WJESUSS <162203970+WJESUSS@users.noreply.github.com>
2024-10-15 09:20:06 -0300

    ForMultiploDeDois

---
+++ exe0046/src/exe0046/foraMultiploDois.java
@@ -0,0 +1,19 @@
+package exe0046;
+
+import java.util.Scanner;
+
+public class foraMultiploDois {
+
+	public static void main(String[] args) {
+	
+		Scanner sc=new Scanner(System.in);
+		
+     int numero = 6;
+     int numeroFinal = 100;
+     
+     for (int i = numero; i <=numeroFinal; i+=2) {
+		System.out.println("A contagem de 6 a 100 é "+i);
+	}
+	}
+
+}
