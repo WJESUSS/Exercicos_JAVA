8582c2da652cc59d634292ac763c4dc530f82642
WJESUSS <162203970+WJESUSS@users.noreply.github.com>
2024-10-23 12:04:53 -0300

    whileexe01

--- exe0056/src/exe0056/while01.java
+++ exe0056/src/exe0056/while01.java
@@ -1,10 +1,31 @@
 package exe0056;
 
+import java.util.Scanner;
+
 public class while01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int numeroSomado = 0;
+		int numero ;
+		
+		do {
+			System.out.println("Digite o numero (1111-para sair)");
+			numero = sc.nextInt();
+			
+			if(numero!=1111) {
+				numeroSomado += numero;
+			}
+		
+		}while(numero!= 1111);
+			System.out.println("A soma dos numero :" +numeroSomado);
 	}
-
 }
+		
+	
+
+
+
+
