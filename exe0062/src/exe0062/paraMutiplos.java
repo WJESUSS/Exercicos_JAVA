7d79de956750f4db2daac72af7c1a88fd5fe7725
WJESUSS <162203970+WJESUSS@users.noreply.github.com>
2024-10-25 14:18:24 -0300

    EstruturaParaSimples

---
+++ exe0062/src/exe0062/paraMutiplos.java
@@ -0,0 +1,25 @@
+package exe0062;
+
+
+import java.util.Scanner;
+
+public class paraMutiplos {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+
+	 
+	 for(int i=0;i<=40;i+=5) {
+		 System.out.print("- " +i+ " ");
+	 }
+	 
+     for(int j=100;j>=0;j-=10) {
+    	 System.out.print("- "+j+" ");
+    	 
+    	 System.out.print("");
+     }
+	}
+
+}
