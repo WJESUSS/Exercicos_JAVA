5f17067ed4ca2bb37a2fbae69696f6e89ae243a8
WJESUSS <162203970+WJESUSS@users.noreply.github.com>
2024-10-27 09:19:18 -0300

    VetorMultiplicação

---
+++ exe0070/src/exe0070/vetorExe.java
@@ -0,0 +1,34 @@
+package exe0070;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class vetorExe {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int[] vetorA = new int[5];
+		int [] vetorB = new int [vetorA.length];
+		
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.print("Entre com o valor do vetor A da posição :" +i+ " = ");
+			vetorA[i]=sc.nextInt();
+			
+			vetorB[i]=vetorA[i]*2;
+		}
+		
+		System.out.println("Vamos imprir a multiplicacão do vetor A :");
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.print(" " +vetorA[i]);
+		}
+		System.out.println();
+		System.out.println("Vamos imprir a multiplicacão do vetor B :");
+		for (int i = 0; i < vetorB.length; i++) {
+			System.out.print(" "+ vetorB[i]);
+		}
+		System.out.println();
+	}
+
+}
