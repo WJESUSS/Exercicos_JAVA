e8ad5a2c58fbbc692d3baa7859f758321ec2b574
WJESUSS <162203970+WJESUSS@users.noreply.github.com>
2024-10-28 16:02:02 -0300

    vetorDados

---
+++ exe0084/src/exe0084/vetorDados.java
@@ -0,0 +1,36 @@
+package exe0084;
+
+import java.util.Scanner;
+
+public class vetorDados {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int idade []=new int[5];
+		String nome [] = new String[5];
+		
+		for (int i = 0; i < nome.length; i++) {
+			System.out.print("Digite seu nome :" +i);
+			nome[i] = sc.nextLine();
+			}
+		for (int i = 0; i < idade.length ; i++) {
+			System.out.print("Digitesua idade  :");
+			idade[i]= sc.nextInt();
+		}
+		
+		
+		System.out.println("Pessoa menores de idade :");
+	
+		for (int i = 0; i < 5; i++) {
+	
+			if(idade[i]<18) {
+				System.out.println("O nome "+nome[i]+ " idade " +idade[i]);
+			}
+		}
+		}
+     
+	
+
+}
