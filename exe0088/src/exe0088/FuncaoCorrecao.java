9c073559704883313e6ecefd172a2edf48c9de9c
WJESUSS <162203970+WJESUSS@users.noreply.github.com>
2024-10-29 10:07:11 -0300

    FunçãoCorrecao

---
+++ exe0088/src/exe0088/FuncaoCorrecao.java
@@ -0,0 +1,30 @@
+package exe0088;
+
+public class FuncaoCorrecao {
+	 public String nome;
+	 
+	 
+	 public void gerador( String mensagem) {
+	    int tamanha = mensagem.length()+12;
+	    String linha  = "+";
+	
+	    for (int i = 0; i < tamanha; i++) {
+			if(i < 7 || i >= tamanha - 7) {
+				linha +="-";
+			}else {
+				linha += "=";
+			}
+		}
+	    linha += "+";
+	    System.out.println(linha); System.out.println("+ " + mensagem + " +"); System.out.println(linha);
+	
+	    
+	}
+	 public  void gerador() {
+		
+	}
+	 
+}
+
+
+
---
+++ exe0088/src/exe0088/executar.java
@@ -0,0 +1,16 @@
+package exe0088;
+
+
+
+public class executar {
+
+	public static void main(String[] args) {
+	
+		FuncaoCorrecao fc = new FuncaoCorrecao();
+		
+		fc.gerador("Aprendendo java");
+	   
+
+	}
+
+}
